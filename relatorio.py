7e59643e544578b8107907d4702011b46420b595
pedrobragaluzpedrobragaluzgit config --global user.name pedrobragaluz <pedroarthurbraga189@gmail.com>
2026-09-20 12:50:33 -0300

    Feito o relaótrio dos alunos usando o mysql connector

---
+++ relatorio.py
@@ -0,0 +1,40 @@
+import mysql.connector
+
+
+conexao = mysql.connector.connect(
+
+    host ="localhost",
+
+    user = "root",
+
+    password = "",
+
+    database ="Projeto_Dev_Poliglota"
+)
+
+cursor = conexao.cursor()
+
+cursor.execute("SELECT id,nome,curso,matricula FROM alunos WHERE matricula != 'Pendente'")
+
+resultados = cursor.fetchall()
+
+
+print("=" * 45)
+
+print("RELATÓRIO FINAL DOS ALUNOS (MÓDULO PYTHON)")
+
+print("=" * 45)
+
+
+for row in resultados:
+    print(f"ID    : {row[0]}")
+
+    print(f"Nome     : {row[1]}")
+
+    print(f"Curso    : {row[2]}")
+
+    print(f"Matrícula    : {row[3]}")
+
+    print("=" * 45)
+
+conexao.close()
